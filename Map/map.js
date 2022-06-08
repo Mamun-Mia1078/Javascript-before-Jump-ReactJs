1dc13ff8b545376cadb34c651c5a79b225d3e492
Mamun Mia <70141554+Mamun-Mia1078@users.noreply.github.com>
2022-06-08 21:30:07 +0600

    Map Practices

---
+++ Map/map.js
@@ -0,0 +1,8 @@
+const favorite = new Map();
+favorite.set('name', 'Muzammel Hoque Mamun');
+favorite.set('Job', 'student');
+favorite.set('color', 'blue');
+favorite.set('os ', 'windows');
+favorite.set('mobile os', 'ios');
+console.log(favorite);
+console.log(favorite.size);
